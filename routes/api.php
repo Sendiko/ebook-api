c4903a0a457019fb1415665931cc17fcc82bdd18
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-18 15:35:21 +0700

    crud to db

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,18 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::get('me', [AuthController::class, 'me']);
+//! option 1
+// ? Route::get('me', [AuthController::class, 'me']);
+// ? Route::get('book', [BookController::class, 'index']);
+// ? Route::get('book/{id}', [BookController::class, 'show']);
+// ? Route::post('book', [BookController::class, 'store']);
+// ? Route::put('book/{id}', [BookController::class, 'update']);
+// ? Route::delete('book/{id}', [BookController::class, 'destroy']);
+
+//! option 2
+Route::resource('books', BookController::class)->except(
+    ['create', 'edit']
+);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
